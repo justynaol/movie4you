2a31027e8d5b45f495567fd4a9d2a90e39ff6bf9
Justyna Oleksiak <justyna-oleksiak@wp.pl>
2021-09-09 20:25:13 +0200

    commit

--- src/main/java/project/movie4you/user/UserEndpoint.java
+++ src/main/java/project/movie4you/user/UserEndpoint.java
@@ -1,9 +1,37 @@
 package project.movie4you.user;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
 public class UserEndpoint {
+    private final UserRepository userRepository;
+
+    public UserEndpoint(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+   /* @PostMapping
+    public ResponseEntity<Void> register(@RequestBody UserDefinition userDefinition) {
+        User user = new User(userDefinition.login, passwordEncoder.encode(userDefinition.password));
+        userRepository.save(user);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+*/
+    @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+    protected static class UserDefinition {
+        String login;
+        String password;
+        public static UserDefinition from(User user) {
+            UserDefinition userDefinition = new UserDefinition();
+            userDefinition.login = user.getLogin();
+            userDefinition.password = user.getPassword();
+            return userDefinition;
+        }
+    }
 }
