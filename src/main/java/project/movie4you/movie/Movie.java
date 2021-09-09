e88e9954125f88728c65b7be738a0615758dd2d3
Justyna Oleksiak <justyna-oleksiak@wp.pl>
2021-09-09 20:08:51 +0200

    commit

--- src/main/java/project/movie4you/movie/Movie.java
+++ src/main/java/project/movie4you/movie/Movie.java
@@ -4,7 +4,7 @@ import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-import java.util.stream.Collectors;
+
 
 import static java.util.stream.Collectors.toList;
 
